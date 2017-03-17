40217e39ab6014e29ecd03a817bbace7db50baf9
Rob Attfield <emailme@robertattfield.com>
2017-03-17 22:28:25 +1300

    Hiding menu items from non-admin users, and also user creation button on users index (for non admin and general users).

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -10,15 +10,19 @@
     <a href="{!! route('users.index') !!}"><i class="fa fa-edit"></i><span>Users</span></a>
 </li>
 
-<li class="{{ Request::is('main-metas*') ? 'active' : '' }}">
-    <a href="{!! route('main-metas.index') !!}"><i class="fa fa-edit"></i><span>Main Meta</span></a>
-</li>
+@if(Auth::user() != null)
+    @if(Auth::user()->is_admin == true)
+        <li class="{{ Request::is('main-metas*') ? 'active' : '' }}">
+            <a href="{!! route('main-metas.index') !!}"><i class="fa fa-edit"></i><span>Main Meta</span></a>
+        </li>
 
-<li class="{{ Request::is('ad-blocks*') ? 'active' : '' }}">
-    <a href="{!! route('ad-blocks.index') !!}"><i class="fa fa-edit"></i><span>Ad Blocks</span></a>
-</li>
+        <li class="{{ Request::is('ad-blocks*') ? 'active' : '' }}">
+            <a href="{!! route('ad-blocks.index') !!}"><i class="fa fa-edit"></i><span>Ad Blocks</span></a>
+        </li>
 
-<li class="{{ Request::is('twitter-configs*') ? 'active' : '' }}">
-    <a href="{!! route('twitter-configs.index') !!}"><i class="fa fa-edit"></i><span>Twitter Config</span></a>
-</li>
+        <li class="{{ Request::is('twitter-configs*') ? 'active' : '' }}">
+            <a href="{!! route('twitter-configs.index') !!}"><i class="fa fa-edit"></i><span>Twitter Config</span></a>
+        </li>
+    @endif
+@endif
 
