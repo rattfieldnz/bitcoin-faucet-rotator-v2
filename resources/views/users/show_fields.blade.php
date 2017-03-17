d668639a51ba5ed9d96408d6c69bd5590b489d71
Rob Attfield <emailme@robertattfield.com>
2017-03-17 21:40:17 +1300

    Added Auth checking for users index (if is_admin).

--- resources/views/users/show_fields.blade.php
+++ resources/views/users/show_fields.blade.php
@@ -1,9 +1,3 @@
-<!-- Id Field -->
-<div class="form-group">
-    {!! Form::label('id', 'Id:') !!}
-    <p>{!! $user->id !!}</p>
-</div>
-
 <!-- User Name Field -->
 <div class="form-group">
     {!! Form::label('user_name', 'User Name:') !!}
@@ -22,41 +16,45 @@
     <p>{!! $user->last_name !!}</p>
 </div>
 
-<!-- Email Field -->
-<div class="form-group">
-    {!! Form::label('email', 'Email:') !!}
-    <p>{!! $user->email !!}</p>
-</div>
+@if(Auth::user() != null)
+    @if(Auth::user()->is_admin == true)
+        <!-- Email Field -->
+        <div class="form-group">
+            {!! Form::label('email', 'Email:') !!}
+            <p>{!! $user->email !!}</p>
+        </div>
 
-<!-- Password Field -->
-<div class="form-group">
-    {!! Form::label('password', 'Password:') !!}
-    <p> ************ </p>
-</div>
+        <!-- Password Field -->
+        <div class="form-group">
+            {!! Form::label('password', 'Password:') !!}
+            <p> ************ </p>
+        </div>
 
-<!-- Bitcoin Address Field -->
-<div class="form-group">
-    {!! Form::label('bitcoin_address', 'Bitcoin Address:') !!}
-    <p>{!! $user->bitcoin_address !!}</p>
-</div>
+        <!-- Bitcoin Address Field -->
+        <div class="form-group">
+            {!! Form::label('bitcoin_address', 'Bitcoin Address:') !!}
+            <p>{!! $user->bitcoin_address !!}</p>
+        </div>
 
-<!-- Is Admin Field -->
-<div class="form-group">
-    {!! Form::label('is_admin', 'Is Admin:') !!}
-    <p>{!! $user->isAnAdmin() !!}</p>
-</div>
+        <!-- Is Admin Field -->
+        <div class="form-group">
+            {!! Form::label('is_admin', 'Is Admin:') !!}
+            <p>{!! $user->isAnAdmin() !!}</p>
+        </div>
 
-<!-- Created At Field -->
-<div class="form-group">
-    {!! Form::label('created_at', 'Created At:') !!}
-    <p>{!! $user->created_at !!}</p>
-</div>
+        <!-- Created At Field -->
+        <div class="form-group">
+            {!! Form::label('created_at', 'Created At:') !!}
+            <p>{!! $user->created_at !!}</p>
+        </div>
 
-<!-- Updated At Field -->
-<div class="form-group">
-    {!! Form::label('updated_at', 'Updated At:') !!}
-    <p>{!! $user->updated_at !!}</p>
-</div>
+        <!-- Updated At Field -->
+        <div class="form-group">
+            {!! Form::label('updated_at', 'Updated At:') !!}
+            <p>{!! $user->updated_at !!}</p>
+        </div>
+    @endif
+@endif
 
 <!-- Slug Field -->
 <div class="form-group">
--- resources/views/users/table.blade.php
+++ resources/views/users/table.blade.php
@@ -4,10 +4,15 @@
         <th>User Name</th>
         <th>First Name</th>
         <th>Last Name</th>
-        <th>Email</th>
-        <th>Password</th>
-        <th>Bitcoin Address</th>
-        <th>Is Admin</th>
+
+        @if(Auth::user() != null)
+            @if(Auth::user()->is_admin == true)
+                <th>Email</th>
+                <th>Password</th>
+                <th>Bitcoin Address</th>
+                <th>Is Admin</th>
+            @endif
+        @endif
         <th>Slug</th>
         <th colspan="3">Action</th>
     </thead>
@@ -17,19 +22,27 @@
             <td>{!! $user->user_name !!}</td>
             <td>{!! $user->first_name !!}</td>
             <td>{!! $user->last_name !!}</td>
-            <td>{!! $user->email !!}</td>
-            <td> ************ </td>
-            <td>{!! $user->bitcoin_address !!}</td>
-            <td>{!! $user->isAnAdmin() !!}</td>
+            @if(Auth::user() != null)
+                @if(Auth::user()->is_admin == true)
+                    <td>{!! $user->email !!}</td>
+                    <td> ************ </td>
+                    <td>{!! $user->bitcoin_address !!}</td>
+                    <td>{!! $user->isAnAdmin() !!}</td>
+                @endif
+            @endif
             <td>{!! $user->slug !!}</td>
             <td>
-                {!! Form::open(['route' => ['users.destroy', $user->slug], 'method' => 'delete']) !!}
                 <div class='btn-group'>
                     <a href="{!! route('users.show', [$user->slug]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                    <a href="{!! route('users.edit', [$user->slug]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                    {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                    @if(Auth::user() != null)
+                        @if(Auth::user()->is_admin == true)
+                            <a href="{!! route('users.edit', [$user->slug]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
+                            {!! Form::open(['route' => ['users.destroy', $user->slug], 'method' => 'delete']) !!}
+                            {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                            {!! Form::close() !!}
+                        @endif
+                    @endif
                 </div>
-                {!! Form::close() !!}
             </td>
         </tr>
     @endforeach
