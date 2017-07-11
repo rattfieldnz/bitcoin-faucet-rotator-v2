2b80478ea1839b853251bae59c4aad413189d315
Robert Attfield <emailme@robertattfield.com>
2017-07-11 23:07:58 +1200

    Added referral spam package.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,6 +37,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Matthewbdaly\ETagMiddleware\ETag::class,
             \Bepsvpt\SecureHeaders\SecureHeadersMiddleware::class,
+            \DougSisk\BlockReferralSpam\Middleware\BlockReferralSpam::class
         ],
 
         'api' => [
