9368b03809b8af64781611ec568bd4f1c474bb0d
Robert Attfield <emailme@robertattfield.com>
2017-07-22 22:25:16 +1200

    Tweaked payment processors table.

--- resources/views/payment_processors/table.blade.php
+++ resources/views/payment_processors/table.blade.php
@@ -7,8 +7,16 @@
             @endif
         @endif
         <th>Name</th>
-        <th>Url</th>
-        <th colspan="3">Action</th>
+        <th>Faucets</th>
+        <th>No. Of Faucets</th>
+        <th>Min. Claimable</th>
+        <th>Max. Claimable</th>
+
+        @if(Auth::user() != null)
+            @if(Auth::user()->isAnAdmin())
+            <th colspan="3">Action</th>
+            @endif
+        @endif
     </thead>
     <tbody>
     @foreach($paymentProcessors as $paymentProcessor)
@@ -18,34 +26,38 @@
                     <td>{!! $paymentProcessor->id !!}</td>
                 @endif
             @endif
-            <td>{!! $paymentProcessor->name !!}</td>
-            <td>{!! $paymentProcessor->url !!}</td>
             <td>
+                {!! link_to_route('payment-processors.show', $paymentProcessor->name, ['slug' => $paymentProcessor->slug]) !!}
+            </td>
+            <td>{!! link_to_route('payment-processors.faucets', $paymentProcessor->name . " Faucets", ['slug' => $paymentProcessor->slug]) !!}</td>
+            <td>{{ count($paymentProcessor->faucets()->get()) }}</td>
+            <td>{{ $paymentProcessor->faucets()->sum('min_payout') }} Satoshis every {{ $paymentProcessor->faucets()->sum('interval_minutes') }} minutes</td>
+            <td>{{ $paymentProcessor->faucets()->sum('max_payout') }} Satoshis every {{ $paymentProcessor->faucets()->sum('interval_minutes') }} minutes</td>
 
+            @if(Auth::user() != null)
+                @if(Auth::user()->isAnAdmin())
+            <td>
                 <div class='btn-group'>
-                    <a href="{!! route('payment-processors.show', ['slug' => $paymentProcessor->slug]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-eye-open"></i></a>
-                    @if(Auth::user() != null)
-                        @if(Auth::user()->is_admin == true && Auth::user()->hasRole('owner'))
-                            <a href="{!! route('payment-processors.edit', ['slug' => $paymentProcessor->slug]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                            @if($paymentProcessor->isDeleted())
-                                {!! Form::open(['route' => ['payment-processors.delete-permanently', $paymentProcessor->slug], 'method' => 'delete']) !!}
-                                {!! csrf_field() !!}
-                                {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure? The payment processor will be PERMANENTLY deleted!')"]) !!}
-                                {!! Form::close() !!}
-                                {!! Form::open(['route' => ['payment-processors.restore', $paymentProcessor->slug], 'method' => 'patch']) !!}
-                                {!! csrf_field() !!}
-                                {!! Form::button('<i class="glyphicon glyphicon-refresh"></i>', ['type' => 'submit', 'class' => 'btn btn-info btn-xs', 'onclick' => "return confirm('Are you sure you want to restore this deleted payment processor?')"]) !!}
-                                {!! Form::close() !!}
-                            @else
-                                {!! Form::open(['route' => ['payment-processors.destroy', 'slug' => $paymentProcessor->slug], 'method' => 'delete']) !!}
-                                {!! csrf_field() !!}
-                                {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-warning btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                                {!! Form::close() !!}
-                            @endif
-                        @endif
+                    <a href="{!! route('payment-processors.edit', ['slug' => $paymentProcessor->slug]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
+                    @if($paymentProcessor->isDeleted())
+                        {!! Form::open(['route' => ['payment-processors.delete-permanently', $paymentProcessor->slug], 'method' => 'delete']) !!}
+                        {!! csrf_field() !!}
+                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure? The payment processor will be PERMANENTLY deleted!')"]) !!}
+                        {!! Form::close() !!}
+                        {!! Form::open(['route' => ['payment-processors.restore', $paymentProcessor->slug], 'method' => 'patch']) !!}
+                        {!! csrf_field() !!}
+                        {!! Form::button('<i class="glyphicon glyphicon-refresh"></i>', ['type' => 'submit', 'class' => 'btn btn-info btn-xs', 'onclick' => "return confirm('Are you sure you want to restore this deleted payment processor?')"]) !!}
+                        {!! Form::close() !!}
+                    @else
+                        {!! Form::open(['route' => ['payment-processors.destroy', 'slug' => $paymentProcessor->slug], 'method' => 'delete']) !!}
+                        {!! csrf_field() !!}
+                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-warning btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                        {!! Form::close() !!}
                     @endif
                 </div>
             </td>
+                @endif
+            @endif
         </tr>
     @endforeach
     </tbody>
