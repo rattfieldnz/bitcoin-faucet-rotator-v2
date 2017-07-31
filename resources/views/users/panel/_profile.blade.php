5a8e9f82c2f3e0bb6002766a0aad9092b2ddf83d
Robert Attfield <emailme@robertattfield.com>
2017-07-31 20:12:52 +1200

    Removed inline styling on user profile page, refactored CSS.

--- resources/views/users/panel/_profile.blade.php
+++ resources/views/users/panel/_profile.blade.php
@@ -5,24 +5,26 @@
                 <img itemprop="image" src="{{ \Helpers\Functions\Users::getGravatar($user) }}" class="img-circle" alt="User Image"/>
             </div>
             <div class="col-xs-9 col-sm-9 col-md-9 col-lg-9" style="vertical-align: middle;">
-                <h2 style="margin: 0 0 0 0.5em;word-wrap: break-word;white-space: normal;">
+                <h2 class="user-profile-heading-h2 word-wrap-white-space">
                     <span itemprop="name" class="user-name">{{ $user->fullName() }}</span>
                 </h2>
-                <h3 style="margin: 0 0 0 0.5em;word-wrap: break-word;white-space: normal;"><small>(<em itemprop="additionalName" class="user-nick">{{ $user->user_name }}</em>)</small></h3>
+                <h3 class="user-profile-heading-h3 word-wrap-white-space">
+                    <small>(<em itemprop="additionalName" class="user-nick">{{ $user->user_name }}</em>)</small>
+                </h3>
             </div>
         </div>
         <div class="vcard-details row">
             @if(!empty(Auth::user()) && (Auth::user() == $user || Auth::user()->isAnAdmin()))
-                <dl title="Email" style="margin-bottom: 0; vertical-align: middle;word-wrap: break-word;white-space: normal;">
-                    <dd style="line-height: 3em; vertical-align: middle;word-wrap: break-word;white-space: normal;">
-                        <i class="fa fa-bitcoin circle-letter" style="margin-right: 0.5em;"></i>
+                <dl title="Email" class="user-email-dl-list word-wrap-white-space">
+                    <dd class="user-profile-dd-list">
+                        <i class="fa fa-bitcoin circle-letter right-margin-half-em"></i>
                         User Email: <a class="email" data-email="{{ $user->email }}" href="mailto:{{ $user->email }}">{{ $user->email }}</a>
                     </dd>
                 </dl>
             @endif
             <dl title="List of Faucets">
-                <dd style="line-height: 3em; vertical-align: middle;word-wrap: break-word;white-space: normal;">
-                    <i class="fa fa-bitcoin circle-letter" style="margin-right: 0.5em;"></i>
+                <dd class="user-profile-dd-list word-wrap-white-space">
+                    <i class="fa fa-bitcoin circle-letter right-margin-half-em"></i>
                     {!!
                         link_to_route(
                             'users.faucets',
@@ -44,8 +46,8 @@
                 </dd>
             </dl>
             <dl title="List of Faucets, Organised by Payment Processors" style="margin-top: -1.5em;">
-                <dd style="line-height: 3em; vertical-align: middle;word-wrap: break-word;white-space: normal;">
-                    <i class="fa fa-bitcoin circle-letter" style="margin-right: 0.5em;"></i>
+                <dd class="user-profile-dd-list word-wrap-white-space">
+                    <i class="fa fa-bitcoin circle-letter right-margin-half-em"></i>
                     {!!
                         link_to_route(
                             'users.payment-processors',
@@ -72,27 +74,25 @@
                 </dd>
             </dl>
             <dl title="Bitcoin Address" style="margin-top: -1.5em;">
-                <dd style="line-height: 3em; vertical-align: middle;">
-                    <i class="fa fa-bitcoin circle-letter" style="margin-right: 0.5em;"></i>
-                    <a
-                            href="https://blockchain.info/address/{{ $user->bitcoin_address }}"
-                            target="_blank" title="{{ $user->user_name }}'s Bitcoin Address"
+                <dd class="user-profile-dd-list word-wrap-white-space">
+                    <i class="fa fa-bitcoin circle-letter right-margin-half-em"></i>
+                    <a href="https://blockchain.info/address/{{ $user->bitcoin_address }}"
+                       target="_blank" title="{{ $user->user_name }}'s Bitcoin Address"
                     >Bitcoin Address</a>
-                    <a
-                            href="https://blockchain.info/address/{{ $user->bitcoin_address }}"
-                            target="_blank" title="{{ $user->user_name }}'s Bitcoin Address"
+                    <a href="https://blockchain.info/address/{{ $user->bitcoin_address }}"
+                       target="_blank" title="{{ $user->user_name }}'s Bitcoin Address"
                     ><i class="fa fa-external-link" style="color: #3c8dbc;"></i></a>
                 </dd>
             </dl>
             <dl title="Registered Date" style="margin-top: -1.5em;">
-                <dd style="line-height: 3em; vertical-align: middle;word-wrap: break-word;white-space: normal;">
-                    <i class="fa fa-bitcoin circle-letter" style="margin-right: 0.5em;"></i>
+                <dd class="user-profile-dd-list word-wrap-white-space">
+                    <i class="fa fa-bitcoin circle-letter right-margin-half-em"></i>
                     Joined on {{ date('l jS \of F Y\, \a\t H:i:s A T', strtotime($user->created_at)) }}
                 </dd>
             </dl>
             <dl title="Profile Last Updated" style="margin-top: -1.5em;">
-                <dd style="line-height: 3em; vertical-align: middle;word-wrap: break-word;white-space: normal;">
-                    <i class="fa fa-bitcoin circle-letter" style="margin-right: 0.5em;"></i>
+                <dd class="user-profile-dd-list word-wrap-white-space">
+                    <i class="fa fa-bitcoin circle-letter right-margin-half-em"></i>
                     Updated profile on {{ date('l jS \of F Y\, \a\t H:i:s A T', strtotime($user->updated_at)) }}
                 </dd>
             </dl>
@@ -104,7 +104,7 @@
                         an
                     @endif admin/owner.</p>
                 <dl title="User Roles">
-                    <p><i class="fa fa-bitcoin circle-letter" style="margin-right: 0.5em;"></i>User Roles: </p>
+                    <p><i class="fa fa-bitcoin circle-letter right-margin-half-em"></i>User Roles: </p>
                     <dd class="roles">
                         <ul>
                             @if(!empty($user))
@@ -116,7 +116,7 @@
                     </dd>
                 </dl>
                 <dl title="User Privileges">
-                    <p><i class="fa fa-bitcoin circle-letter" style="margin-right: 0.5em;"></i>User Privileges: </p>
+                    <p><i class="fa fa-bitcoin circle-letter right-margin-half-em"></i>User Privileges: </p>
                     @if(!empty($user) && count($user->permissions) > 0)
                         <dd class="privileges">
                             <ul>
