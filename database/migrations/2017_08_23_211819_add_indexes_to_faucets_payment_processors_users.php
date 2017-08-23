39aa1b81c420bc8a1a141954ba86d901d46cb15a
Robert Attfield <emailme@robertattfield.com>
2017-08-23 22:31:40 +1200

    Adding index columns to users, faucets, payment_processors tables.

---
+++ database/migrations/2017_08_23_211819_add_indexes_to_faucets_payment_processors_users.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIndexesToFaucetsPaymentProcessorsUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('faucets', function (Blueprint $table) {
+            $table->unique(['id', 'slug']);
+            $table->index('name');
+            $table->unique('url');
+            $table->index('interval_minutes');
+            $table->index('min_payout');
+            $table->index('max_payout');
+            $table->index('has_ref_program');
+            $table->index('ref_payout_percent');
+            $table->index('is_paused');
+        });
+
+        Schema::table('payment_processors', function (Blueprint $table) {
+            $table->unique(['id', 'slug']);
+            $table->unique('url');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->index(['id', 'slug']);
+            $table->index('user_name');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('faucets', function (Blueprint $table) {
+            $table->dropIndex('id');
+            $table->dropIndex('slug');
+            $table->dropIndex('name');
+            $table->dropIndex('url');
+            $table->dropIndex('interval_minutes');
+            $table->dropIndex('min_payout');
+            $table->dropIndex('max_payout');
+            $table->dropIndex('has_ref_program');
+            $table->dropIndex('ref_payout_percent');
+            $table->dropIndex('is_paused');
+        });
+
+        Schema::table('payment_processors', function (Blueprint $table) {
+            $table->dropIndex('id');
+            $table->dropIndex('slug');
+            $table->dropIndex('url');
+        });
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropIndex('id');
+            $table->dropIndex('slug');
+            $table->dropIndex('user_name');
+        });
+    }
+}
