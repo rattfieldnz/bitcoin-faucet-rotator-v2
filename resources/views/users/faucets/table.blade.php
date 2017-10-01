67ad644fdecefa3d89d4814d57f10f81d5059a8b
Robert Attfield <emailme@robertattfield.com>
2017-10-01 21:30:06 +1300

    Refactored code.

--- resources/views/users/faucets/table.blade.php
+++ resources/views/users/faucets/table.blade.php
@@ -74,29 +74,6 @@
 <script>
     $(function () {
 
-        $.ajaxSetup({
-            timeout: 3600000,
-
-            // force ajax call on all browsers
-            cache: false,
-
-            // Enables cross domain requests
-            crossDomain: true,
-
-            // Helps in setting cookie
-            xhrFields: {
-                withCredentials: true
-            },
-
-            beforeSend: function (xhr, type) {
-                // Set the CSRF Token in the header for security
-                //if (type.type !== "GET") {
-                var token = Cookies.get("XSRF-TOKEN");
-                xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-                xhr.setRequestHeader('X-XSRF-Token', token);
-                //}
-            }
-        });
         $.fn.dataTable.ext.errMode = 'none';
 
         //--------------------------------
