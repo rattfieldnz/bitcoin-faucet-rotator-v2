ac6918c40fa045ad198f2260b18034dc41e463c4
Robert Attfield <emailme@robertattfield.com>
2017-10-01 19:56:08 +1300

    Refactored code.

--- resources/views/payment_processors/table.blade.php
+++ resources/views/payment_processors/table.blade.php
@@ -1,87 +1,87 @@
-<div class="table-responsive">
-<table class="table table-striped bordered tablesorter" id="payment-processors-table">
-    <thead class="row">
-        @if(Auth::user() != null)
-            @if(Auth::user()->isAnAdmin())
-                <th>Id</th>
-            @endif
-        @endif
-        <th>Name</th>
-        <th>Faucets</th>
-        <th>Rotators</th>
-        <th>No. Of Faucets</th>
-        <th>Min. Claimable</th>
-        <th>Max. Claimable</th>
+<p><strong>*</strong> Payout amounts are in Satoshis</p>
 
-        @if(Auth::user() != null)
-            @if(Auth::user()->isAnAdmin())
-            <th colspan="3">Action</th>
-            @endif
-        @endif
-    </thead>
-    <tbody class="row">
-    @foreach($paymentProcessors as $paymentProcessor)
+<div id="paymentProcessorsTable-progressbar" role="progressbar" aria-valuemin="0" aria-valuemax="100"></div>
+<div></div>
+
+@if(Route::currentRouteName() != 'payment-processors.index')
+    @if(!empty(Auth::user() && Auth::user()->isAnAdmin()))
+
+        {!! Form::button(
+            '<i class="fa fa-2x fa-plus" style="vertical-align: middle; margin-right:0.25em;"></i>Add New Faucet',
+            [
+                'type' => 'button',
+                'onClick' => "location.href='" . route('payment-processors.create') . "'",
+                'class' => 'btn btn-success col-lg-2 col-md-2 col-sm-2 col-xs-12',
+                'style' => 'margin:0.25em 0 0.25em 0; color: white; min-width:12em;'
+            ])
+        !!}
+    @endif
+@endif
+<div class="chart">
+
+    <table id="paymentProcessorsTable"
+           class="row-border hover order-column {{ !Auth::check() ? 'paymentProcessorsTable_guest' : '' }}"
+           cellspacing="0"
+           width="100%">
+        <thead>
         <tr>
             @if(Auth::user() != null)
-                @if(Auth::user()->is_admin == true && Auth::user()->hasRole('owner'))
-                    <td>{!! $paymentProcessor->id !!}</td>
+                @if(Auth::user()->isAnAdmin())
+                    <th>Id</th>
                 @endif
             @endif
-            <td>
-                {!! link_to_route('payment-processors.show', $paymentProcessor->name, ['slug' => $paymentProcessor->slug]) !!}
-            </td>
-            <td>{!! link_to_route('payment-processors.faucets', $paymentProcessor->name . " Faucets", ['slug' => $paymentProcessor->slug]) !!}</td>
-            <td>
-                {!! link_to_route('payment-processors.rotator', $paymentProcessor->name . ' Rotator', ['slug' => $paymentProcessor->slug]) !!}
-            </td>
-            <td>
-                @if(!empty(Auth::user()) && Auth::user()->isAnAdmin())
-                    {{ count($paymentProcessor->faucets()->withTrashed()->get()) }}
-                @else
-                    {{ count($paymentProcessor->faucets()->get()) }}
-                @endif
-            </td>
-
-            @if(count($paymentProcessor->faucets()->get()) != 0)
-                <td>
-                    {{ $paymentProcessor->faucets()->sum('min_payout') }}
-                    Satoshis / {{ $paymentProcessor->faucets()->sum('interval_minutes') }} minutes
-                </td>
-                <td>
-                    {{ $paymentProcessor->faucets()->sum('max_payout') }}
-                    Satoshis / {{ $paymentProcessor->faucets()->sum('interval_minutes') }} minutes
-                </td>
-            @else
-                <td>N/A</td>
-                <td>N/A</td>
-            @endif
-
+            <th>Name</th>
+            <th>Faucets</th>
+            <th>Rotators</th>
+            <th>No. of Faucets</th>
+            <th>Min. Claimable</th>
+            <th>Max. Claimable</th>
             @if(Auth::user() != null)
                 @if(Auth::user()->isAnAdmin())
-                <td>
-                <div class='btn-group'>
-                    <a href="{!! route('payment-processors.edit', ['slug' => $paymentProcessor->slug]) !!}" class='btn btn-default btn-xs'><i class="glyphicon glyphicon-edit"></i></a>
-                    @if($paymentProcessor->isDeleted())
-                        {!! Form::open(['route' => ['payment-processors.delete-permanently', $paymentProcessor->slug], 'method' => 'delete']) !!}
-                        {!! csrf_field() !!}
-                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure? The payment processor will be PERMANENTLY deleted!')"]) !!}
-                        {!! Form::close() !!}
-                        {!! Form::open(['route' => ['payment-processors.restore', $paymentProcessor->slug], 'method' => 'patch']) !!}
-                        {!! csrf_field() !!}
-                        {!! Form::button('<i class="glyphicon glyphicon-refresh"></i>', ['type' => 'submit', 'class' => 'btn btn-info btn-xs', 'onclick' => "return confirm('Are you sure you want to restore this deleted payment processor?')"]) !!}
-                        {!! Form::close() !!}
-                    @else
-                        {!! Form::open(['route' => ['payment-processors.destroy', 'slug' => $paymentProcessor->slug], 'method' => 'delete']) !!}
-                        {!! csrf_field() !!}
-                        {!! Form::button('<i class="glyphicon glyphicon-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-warning btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
-                        {!! Form::close() !!}
-                    @endif
-                </div>
-            </td>
+                    <th>Action</th>
                 @endif
             @endif
         </tr>
-    @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody></tbody>
+        <tfoot>
+        <tr>
+            @if(Auth::user() != null)
+                @if(Auth::user()->isAnAdmin())
+                    <th>Id</th>
+                @endif
+            @endif
+            <th>Name</th>
+            <th>Faucets</th>
+            <th>Rotators</th>
+            <th>No. of Faucets</th>
+            <th>Min. Claimable</th>
+            <th>Max. Claimable</th>
+            @if(Auth::user() != null)
+                @if(Auth::user()->isAnAdmin())
+                    <th>Action</th>
+                @endif
+            @endif
+        </tr>
+        </tfoot>
+    </table>
 </div>
+
+@push('scripts')
+<script src="/assets/js/datatables.net/datatables.min.js?{{ rand() }}"></script>
+<script src="/assets/js/payment-processor-scripts/paymentProcessorDatatables.js?{{ rand() }}"></script>
+<script>
+    $(function () {
+        $.fn.dataTable.ext.errMode = 'none';
+
+        //--------------------------------
+        //- DATATABLES SHOWING FAUCETS -
+        //--------------------------------
+        var dataTablesName = 'payment processors datatable';
+        var route = laroute.route('payment-processors');
+        var faucetsData = getFaucetsDataAjax(route);
+        var paymentProcessorsTableProgressBar = generateProgressBar("#paymentProcessorsTable-progressbar",dataTablesName);
+        renderPaymentProcessorsDataTable(faucetsData, '#paymentProcessorsTable', paymentProcessorsTableProgressBar);
+    });
+</script>
+@endpush
