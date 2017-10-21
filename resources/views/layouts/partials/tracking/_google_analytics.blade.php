af08574753a8e46e25ee8cd05b1cf88dba17bbcc
Robert Attfield <emailme@robertattfield.com>
2017-10-21 21:16:54 +1300

    Updated Google Analytics tracking code.

--- resources/views/layouts/partials/tracking/_google_analytics.blade.php
+++ resources/views/layouts/partials/tracking/_google_analytics.blade.php
@@ -5,12 +5,14 @@ $mainMeta = MainMeta::firstOrFail();
 ?>
 
 @if(!empty($mainMeta->google_analytics_id))
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ $mainMeta->google_analytics_id }}"></script>
 <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', '{{ $mainMeta->google_analytics_id }}', 'auto');
-    ga('send', 'pageview');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', '{{ $mainMeta->google_analytics_id }}');
 </script>
+
 @endif
