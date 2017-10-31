ad09fe6c8c186be23f361eff54aeb819da7d989a
Robert Attfield <emailme@robertattfield.com>
2017-11-01 11:27:42 +1300

    PSR fixes.

--- app/Helpers/Social/SocialNetworkLinks.php
+++ app/Helpers/Social/SocialNetworkLinks.php
@@ -16,45 +16,45 @@ class SocialNetworkLinks
     /**
      * @return array
      */
-    public static function adminLinks(){
+    public static function adminLinks()
+    {
 
         $adminUser = Users::adminUser();
         $socialLinks = $adminUser->socialNetworkLinks()->first();
 
         $existingLinks = [];
 
-        if(!empty($socialLinks->facebook_url)){
+        if (!empty($socialLinks->facebook_url)) {
             $existingLinks['facebook'] = $socialLinks->facebook_url;
         }
-        if(!empty($socialLinks->twitter_url)){
+        if (!empty($socialLinks->twitter_url)) {
             $existingLinks['twitter'] = $socialLinks->twitter_url;
         }
-        if(!empty($socialLinks->reddit_url)){
+        if (!empty($socialLinks->reddit_url)) {
             $existingLinks['reddit'] = $socialLinks->reddit_url;
         }
-        if(!empty($socialLinks->google_plus_url)){
+        if (!empty($socialLinks->google_plus_url)) {
             $existingLinks['google-plus'] = $socialLinks->google_plus_url;
         }
-        if(!empty($socialLinks->youtube_url)){
+        if (!empty($socialLinks->youtube_url)) {
             $existingLinks['youtube'] = $socialLinks->youtube_url;
         }
-        if(!empty($socialLinks->tumblr_url)){
+        if (!empty($socialLinks->tumblr_url)) {
             $existingLinks['tumblr'] = $socialLinks->tumblr_url;
         }
-        if(!empty($socialLinks->vimeo_url)){
+        if (!empty($socialLinks->vimeo_url)) {
             $existingLinks['vimeo'] = $socialLinks->vimeo_url;
         }
-        if(!empty($socialLinks->vkontakte_url)){
+        if (!empty($socialLinks->vkontakte_url)) {
             $existingLinks['vk'] = $socialLinks->vkontakte_url;
         }
-        if(!empty($socialLinks->sinaweibo_url)){
+        if (!empty($socialLinks->sinaweibo_url)) {
             $existingLinks['weibo'] = $socialLinks->sinaweibo_url;
         }
-        if(!empty($socialLinks->xing_url)){
+        if (!empty($socialLinks->xing_url)) {
             $existingLinks['xing'] = $socialLinks->xing_url;
         }
 
         return $existingLinks;
-
     }
 }
