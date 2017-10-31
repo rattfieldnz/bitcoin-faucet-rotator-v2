c4c6df1b4fc7c9edbc055025a3dedd343bad6332
Robert Attfield <emailme@robertattfield.com>
2017-11-01 11:25:39 +1300

    Refactored navigation for guest visitors.

--- resources/views/layouts/partials/content/_guest_content.blade.php
+++ resources/views/layouts/partials/content/_guest_content.blade.php
@@ -20,14 +20,31 @@
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul id="navigation" class="nav navbar-nav">
-                    <li><a href="/">Home</a></li>
-                    <li>{!! link_to_route('faucets.index', "Faucets") !!}</li>
-                    <li>{!! link_to_route('payment-processors.index', "Payment Processors") !!}</li>
-                    <li>{!! link_to_route('users.index', "Current Users") !!}</li>
-                    <li>{!! link_to_route('privacy-policy', "Privacy Policy") !!}</li>
-                    <li>{!! link_to_route('terms-and-conditions', "Terms &amp; Conditions") !!}</li>
-                    <li><a href="{!! url('/login') !!}">Login</a></li>
-                    <li><a href="{!! url('/register') !!}">Register</a></li>
+                    <li class="standard-nav-item"><a href="/">Home</a></li>
+                    <li class="standard-nav-item">{!! link_to_route('faucets.index', "Faucets") !!}</li>
+                    <li class="standard-nav-item">{!! link_to_route('payment-processors.index', "Payment Processors") !!}</li>
+                    <li class="standard-nav-item">{!! link_to_route('users.index', "Users") !!}</li>
+                    <li class="standard-nav-item">{!! link_to_route('privacy-policy', "Privacy Policy") !!}</li>
+                    <li class="standard-nav-item">{!! link_to_route('terms-and-conditions', "Terms &amp; Conditions") !!}</li>
+                    <li class="dropdown standard-nav-item">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Get Social! <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            @foreach(\App\Helpers\Social\SocialNetworkLinks::adminLinks() as $socialNetworkName => $link)
+                                <li class="sub-nav-item">
+                                    <a
+                                        href="{{ $link }}"
+                                        title="{{ ucwords(str_replace('-', ' ', $socialNetworkName)) }}"
+                                        target="_blank"
+                                    >
+                                        <i class="fa fa-{{ $socialNetworkName }} fa-2x pull-left sub-nav-icon" aria-hidden="true"></i>
+                                        <span class="pull-right sub-nav-text">{{ ucwords(str_replace('-', ' ', $socialNetworkName)) }}</span>
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </li>
+                    <li class="standard-nav-item"><a href="{!! url('/login') !!}">Login</a></li>
+                    <li class="standard-nav-item"><a href="{!! url('/register') !!}">Register</a></li>
                 </ul>
             </div>
         </div>
