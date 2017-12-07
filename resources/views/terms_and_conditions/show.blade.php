606c266034913c845b22435a0739d848047a93a1
Robert Attfield <emailme@robertattfield.com>
2017-12-07 14:05:26 +1300

    Altered title heading for terms and conditions.

--- resources/views/terms_and_conditions/show.blade.php
+++ resources/views/terms_and_conditions/show.blade.php
@@ -6,22 +6,19 @@
             <h1>Terms and Conditions</h1>
             @include('layouts.partials.social.addthis')
         </div>
+        @if(!empty(Auth::user()) && Auth::user()->isAnAdmin())
         <div class="row zero-margin buttons-row">
-            @if(Auth::user() != null)
-                @if(Auth::user()->isAnAdmin())
-
-                    {!! Form::button(
-                        '<i class="fa fa-2x fa-edit" style="vertical-align: middle; margin-right:0.25em;"></i>Edit Terms &amp; Conditions',
-                        [
-                            'type' => 'button',
-                            'onClick' => "location.href='terms-and-conditions/edit'",
-                            'class' => 'btn btn-primary col-lg-2 col-md-2 col-sm-2 col-xs-12',
-                            'style' => 'margin:0.25em 0 0 0; color: white; min-width:14em;'
-                        ])
-                    !!}
-                @endif
-            @endif
+            {!! Form::button(
+                '<i class="fa fa-2x fa-edit" style="vertical-align: middle; margin-right:0.25em;"></i>Edit Terms &amp; Conditions',
+                [
+                    'type' => 'button',
+                    'onClick' => "location.href='terms-and-conditions/edit'",
+                    'class' => 'btn btn-primary col-lg-2 col-md-2 col-sm-2 col-xs-12',
+                    'style' => 'margin:0.25em 0 0 0; color: white; min-width:14em;'
+                ])
+            !!}
         </div>
+        @endif
     </section>
     <div class="content {{ empty(Auth::user()) ? 'content-guest' : '' }}">
         <div class="clearfix"></div>
