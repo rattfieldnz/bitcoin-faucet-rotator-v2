4af0d0a599683e7bc459452c1af1c5c1e96e0674
Robert Attfield <emailme@robertattfield.com>
2017-12-09 17:02:51 +1300

    Adding alerts index to alerts sitemap routes.

--- routes/sitemaps/sitemap-alerts.php
+++ routes/sitemaps/sitemap-alerts.php
@@ -7,6 +7,8 @@ Route::get('sitemap-alerts', function() {
 
         $alerts = \App\Models\Alert::where('deleted_at', '=', null)->get();
 
+        $sitemap->add(route('alerts.index'), \Carbon\Carbon::now()->toW3cString(), '1.0', 'daily');
+
         foreach($alerts as $a){
             $url = route('alerts.show', ['slug' => $a->slug]);
             $sitemap->add($url, $a->updated_at->toW3cString(), '1.0', 'daily');
