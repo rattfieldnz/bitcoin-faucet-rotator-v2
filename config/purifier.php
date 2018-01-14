74e5111e52c1586ba8789d1eb21d54201d06df42
Robert Attfield <emailme@robertattfield.com>
2018-01-14 21:35:13 +1300

    Added href attribute in purifier config.

--- config/purifier.php
+++ config/purifier.php
@@ -25,7 +25,7 @@ return [
         "default" => [
             'HTML.Doctype' => 'HTML 4.01 Transitional',
             'HTML.Allowed' => 'div[class|style|id],b,strong,i[class|style|id]'
-                . ',em,a[class|style|id|title|target],ul[class|style|id],ol[class|style|id],li[class|style|id]'
+                . ',em,a[class|style|id|title|target|href],ul[class|style|id],ol[class|style|id],li[class|style|id]'
                 . ',p[class|style|id],br,span[class|style|id],img[width|height|alt|src|class|style|id]'
                 . ',iframe[src|scrolling|class|style|id],h1[class|style|id],h2[class|style|id],h3[class|style|id],h4[class|style|id],h5[class|style|id],h6[class|style|id]'
                 . ',dt[class|style|id],dl[class|style|id]',
