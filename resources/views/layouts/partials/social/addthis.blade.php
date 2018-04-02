d446fdb729ee985c8c20256c8044261c6459197a
Robert Attfield <emailme@robertattfield.com>
2018-04-02 19:51:20 +1200

    Minor tweak to AddThis inline sharing buttons.

--- resources/views/layouts/partials/social/addthis.blade.php
+++ resources/views/layouts/partials/social/addthis.blade.php
@@ -1,6 +1,6 @@
 @if(!empty(\App\Helpers\WebsiteMeta\WebsiteMeta::addThisId()))
     <p style="margin-left:0.25em;">
-        <span id="addthis" class="addthis_sharing_toolbox"></span>
+        <span id="addthis" class="addthis_inline_share_toolbox"></span>
         @if(Route::currentRouteName() != 'home' & Route::currentRouteName() != 'login' & Route::currentRouteName() != 'register')
         <i class="fa fa-comments fa-2x" aria-hidden="true"></i> (<a href="#disqus_thread">see comments</a>)
         @endif
