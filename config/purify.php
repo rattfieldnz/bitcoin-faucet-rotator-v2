79fffc325b1fe78babfe0f22766144e5d8d66497
Robert Attfield <emailme@robertattfield.com>
2019-06-07 21:17:18 +1200

    Testing Purify config values.

--- config/purify.php
+++ config/purify.php
@@ -18,6 +18,17 @@ return [
 
     'Cache.SerializerPath' => storage_path('purify'),
 
+    'HTML.Allowed' => 'div[class|style|id],b,strong,i[class|style|id]'
+        . ',em,a[class|style|id|title|target|href],ul[class|style|id],ol[class|style|id],li[class|style|id]'
+        . ',p[class|style|id],br,span[class|style|id],img[width|height|alt|src|class|style|id]'
+        . ',iframe[src|scrolling|class|style|id|data-aa],h1[class|style|id],h2[class|style|id],h3[class|style|id],h4[class|style|id],h5[class|style|id],h6[class|style|id]'
+        . ',dt[class|style|id],dl[class|style|id]',
+
+    'custom_attributes' => [
+        ['a', 'target', 'Enum#_blank,_self,_target,_top'],
+        ['iframe', 'data-aa', 'Text'],
+    ],
+
     'settings' => [
 
         /*
