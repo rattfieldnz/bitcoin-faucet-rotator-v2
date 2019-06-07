8d88da06cb6a43296a7de210fb24697ff769f2a8
Robert Attfield <emailme@robertattfield.com>
2019-06-07 20:54:53 +1200

    Testing Purify config values.

--- config/purify.php
+++ config/purify.php
@@ -17,87 +17,87 @@
  */
 
 return [
-    'Core.Encoding'      => 'UTF-8',
-    'finalize'      => true,
-    'Cache.SerializerPath'     => storage_path('purifier'),
-    'cacheFileMode' => 0755,
-    'HTML.Doctype'             => 'HTML 4.01 Transitional',
-    'HTML.Allowed' => 'div[class|style|id],b,strong,i[class|style|id]'
-        . ',em,a[class|style|id|title|target|href],ul[class|style|id],ol[class|style|id],li[class|style|id]'
-        . ',p[class|style|id],br,span[class|style|id],img[width|height|alt|src|class|style|id]'
-        . ',iframe[src|scrolling|class|style|id|data-aa],h1[class|style|id],h2[class|style|id],h3[class|style|id],h4[class|style|id],h5[class|style|id],h6[class|style|id]'
-        . ',dt[class|style|id],dl[class|style|id]',
-    'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
-    'AutoFormat.AutoParagraph' => true,
-    'AutoFormat.RemoveEmpty'   => true,
-    'Attr.EnableID' => 'true',
-    "HTML.SafeIframe"      => 'true',
-    "URI.SafeIframeRegexp" => "%^(http://|https://|//)(www.youtube.com/embed/|player.vimeo.com/video/)%",
-    'custom_definition' => [
-        'id'  => 'html5-definitions',
-        'rev' => 1,
-        'debug' => false,
-        'elements' => [
-            // http://developers.whatwg.org/sections.html
-            ['section', 'Block', 'Flow', 'Common'],
-            ['nav',     'Block', 'Flow', 'Common'],
-            ['article', 'Block', 'Flow', 'Common'],
-            ['aside',   'Block', 'Flow', 'Common'],
-            ['header',  'Block', 'Flow', 'Common'],
-            ['footer',  'Block', 'Flow', 'Common'],
+    'settings' => [
+        'Core.Encoding'      => 'UTF-8',
+        'finalize'      => true,
+        'Cache.SerializerPath'     => storage_path('purify'),
+        'cacheFileMode' => 0755,
+        'HTML.Doctype'             => 'HTML 4.01 Transitional',
+        'HTML.Allowed' => 'div[class|style|id],b,strong,i[class|style|id]'
+            . ',em,a[class|style|id|title|target|href],ul[class|style|id],ol[class|style|id],li[class|style|id]'
+            . ',p[class|style|id],br,span[class|style|id],img[width|height|alt|src|class|style|id]'
+            . ',iframe[src|scrolling|class|style|id|data-aa],h1[class|style|id],h2[class|style|id],h3[class|style|id],h4[class|style|id],h5[class|style|id],h6[class|style|id]'
+            . ',dt[class|style|id],dl[class|style|id]',
+        'CSS.AllowedProperties'    => 'font,font-size,font-weight,font-style,font-family,text-decoration,padding-left,color,background-color,text-align',
+        'AutoFormat.AutoParagraph' => true,
+        'AutoFormat.RemoveEmpty'   => true,
+        'Attr.EnableID' => 'true',
+        "HTML.SafeIframe"      => 'true',
+        "URI.SafeIframeRegexp" => "%^(http://|https://|//)(www.youtube.com/embed/|player.vimeo.com/video/)%",
+        'custom_definition' => [
+            'id'  => 'html5-definitions',
+            'rev' => 1,
+            'debug' => false,
+            'elements' => [
+                // http://developers.whatwg.org/sections.html
+                ['section', 'Block', 'Flow', 'Common'],
+                ['nav',     'Block', 'Flow', 'Common'],
+                ['article', 'Block', 'Flow', 'Common'],
+                ['aside',   'Block', 'Flow', 'Common'],
+                ['header',  'Block', 'Flow', 'Common'],
+                ['footer',  'Block', 'Flow', 'Common'],
 
-            // Content model actually excludes several tags, not modelled here
-            ['address', 'Block', 'Flow', 'Common'],
-            ['hgroup', 'Block', 'Required: h1 | h2 | h3 | h4 | h5 | h6', 'Common'],
+                // Content model actually excludes several tags, not modelled here
+                ['address', 'Block', 'Flow', 'Common'],
+                ['hgroup', 'Block', 'Required: h1 | h2 | h3 | h4 | h5 | h6', 'Common'],
 
-            // http://developers.whatwg.org/grouping-content.html
-            ['figure', 'Block', 'Optional: (figcaption, Flow) | (Flow, figcaption) | Flow', 'Common'],
-            ['figcaption', 'Inline', 'Flow', 'Common'],
+                // http://developers.whatwg.org/grouping-content.html
+                ['figure', 'Block', 'Optional: (figcaption, Flow) | (Flow, figcaption) | Flow', 'Common'],
+                ['figcaption', 'Inline', 'Flow', 'Common'],
 
-            // http://developers.whatwg.org/the-video-element.html#the-video-element
-            ['video', 'Block', 'Optional: (source, Flow) | (Flow, source) | Flow', 'Common', [
-                'src' => 'URI',
-                'type' => 'Text',
-                'width' => 'Length',
-                'height' => 'Length',
-                'poster' => 'URI',
-                'preload' => 'Enum#auto,metadata,none',
-                'controls' => 'Bool',
-            ]],
-            ['source', 'Block', 'Flow', 'Common', [
-                'src' => 'URI',
-                'type' => 'Text',
-            ]],
+                // http://developers.whatwg.org/the-video-element.html#the-video-element
+                ['video', 'Block', 'Optional: (source, Flow) | (Flow, source) | Flow', 'Common', [
+                    'src' => 'URI',
+                    'type' => 'Text',
+                    'width' => 'Length',
+                    'height' => 'Length',
+                    'poster' => 'URI',
+                    'preload' => 'Enum#auto,metadata,none',
+                    'controls' => 'Bool',
+                ]],
+                ['source', 'Block', 'Flow', 'Common', [
+                    'src' => 'URI',
+                    'type' => 'Text',
+                ]],
 
-            // http://developers.whatwg.org/text-level-semantics.html
-            ['s',    'Inline', 'Inline', 'Common'],
-            ['var',  'Inline', 'Inline', 'Common'],
-            ['sub',  'Inline', 'Inline', 'Common'],
-            ['sup',  'Inline', 'Inline', 'Common'],
-            ['mark', 'Inline', 'Inline', 'Common'],
-            ['wbr',  'Inline', 'Empty', 'Core'],
+                // http://developers.whatwg.org/text-level-semantics.html
+                ['s',    'Inline', 'Inline', 'Common'],
+                ['var',  'Inline', 'Inline', 'Common'],
+                ['sub',  'Inline', 'Inline', 'Common'],
+                ['sup',  'Inline', 'Inline', 'Common'],
+                ['mark', 'Inline', 'Inline', 'Common'],
+                ['wbr',  'Inline', 'Empty', 'Core'],
 
-            // http://developers.whatwg.org/edits.html
-            ['ins', 'Block', 'Flow', 'Common', ['cite' => 'URI', 'datetime' => 'CDATA']],
-            ['del', 'Block', 'Flow', 'Common', ['cite' => 'URI', 'datetime' => 'CDATA']],
+                // http://developers.whatwg.org/edits.html
+                ['ins', 'Block', 'Flow', 'Common', ['cite' => 'URI', 'datetime' => 'CDATA']],
+                ['del', 'Block', 'Flow', 'Common', ['cite' => 'URI', 'datetime' => 'CDATA']],
+            ],
+            'attributes' => [
+                ['iframe', 'allowfullscreen', 'Bool'],
+                ['table', 'height', 'Text'],
+                ['td', 'border', 'Text'],
+                ['th', 'border', 'Text'],
+                ['tr', 'width', 'Text'],
+                ['tr', 'height', 'Text'],
+                ['tr', 'border', 'Text'],
+            ],
         ],
-        'attributes' => [
-            ['iframe', 'allowfullscreen', 'Bool'],
-            ['table', 'height', 'Text'],
-            ['td', 'border', 'Text'],
-            ['th', 'border', 'Text'],
-            ['tr', 'width', 'Text'],
-            ['tr', 'height', 'Text'],
-            ['tr', 'border', 'Text'],
+        'custom_attributes' => [
+            ['a', 'target', 'Enum#_blank,_self,_target,_top'],
         ],
-    ],
-    'custom_attributes' => [
-        ['a', 'target', 'Enum#_blank,_self,_target,_top'],
-    ],
-    'custom_elements' => [
-        ['u', 'Inline', 'Inline', 'Common'],
-    ],
-    'settings'      => [
+        'custom_elements' => [
+            ['u', 'Inline', 'Inline', 'Common'],
+        ],
     ],
 
 ];
