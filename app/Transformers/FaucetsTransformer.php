8898a8b8a8e192d56933c904e5aeb31e31abfa42
Robert Attfield <emailme@robertattfield.com>
2020-05-08 15:19:56 +1200

    Fixed minor bug, updated Composer packages.

--- app/Transformers/FaucetsTransformer.php
+++ app/Transformers/FaucetsTransformer.php
@@ -30,34 +30,38 @@ class FaucetsTransformer extends TransformerAbstract
      */
     public function transform(User $user, Faucet $model, $addPaymentProcessors = false)
     {
-        $referralCode = Faucets::getUserFaucetRefCode($user, $model);
-        $faucet = [
-            'name' => $model->name,
-            'slug' => $model->slug,
-            'url' => $model->url . $referralCode,
-            'can_show_in_iframes' => (boolean)Http::canShowInIframes($model->url),
-            'interval_minutes' => (int)$model->interval_minutes,
-            'min_payout' => (int)$model->min_payout,
-            'max_payout' => (int)$model->max_payout,
-            'has_ref_program' => (boolean)$model->has_ref_program,
-            'ref_payout_percent' => (double)$model->ref_payout_percent,
-            'comments' => $model->comments,
-            'is_paused' => (boolean)$model->is_paused,
-            'meta_title' => $model->meta_title,
-            'meta_description' => $model->meta_description,
-            'meta_keywords' => $model->meta_keywords,
-            'user_slug' => $user->slug
-        ];
+        if(!empty($user) && !empty($model)){
+            $referralCode = Faucets::getUserFaucetRefCode($user, $model);
+            $faucet = [
+                'name' => $model->name,
+                'slug' => $model->slug,
+                'url' => $model->url . $referralCode,
+                'can_show_in_iframes' => (boolean)Http::canShowInIframes($model->url),
+                'interval_minutes' => (int)$model->interval_minutes,
+                'min_payout' => (int)$model->min_payout,
+                'max_payout' => (int)$model->max_payout,
+                'has_ref_program' => (boolean)$model->has_ref_program,
+                'ref_payout_percent' => (double)$model->ref_payout_percent,
+                'comments' => $model->comments,
+                'is_paused' => (boolean)$model->is_paused,
+                'meta_title' => $model->meta_title,
+                'meta_description' => $model->meta_description,
+                'meta_keywords' => $model->meta_keywords,
+                'user_slug' => $user->slug
+            ];
 
-        if ($addPaymentProcessors == true) {
-            $paymentProcessors = $model->paymentProcessors()->get();
-            for ($i = 0; $i < count($paymentProcessors); $i++) {
-                $paymentProcessors[$i] = (new PaymentProcessorsTransformer)->transform($paymentProcessors[$i]);
+            if ($addPaymentProcessors == true) {
+                $paymentProcessors = $model->paymentProcessors()->get();
+                for ($i = 0; $i < count($paymentProcessors); $i++) {
+                    $paymentProcessors[$i] = (new PaymentProcessorsTransformer)->transform($paymentProcessors[$i]);
+                }
+                $faucet['payment_processors'] = $paymentProcessors;
             }
-            $faucet['payment_processors'] = $paymentProcessors;
-        }
 
-        return $faucet;
+            return $faucet;
+        } else {
+            return [];
+        }
     }
 
     public function transformMultiple(Collection $models)
